40b09f3299cbfd0437b640ce83f529a3f4f1c787
TobiasHarrill <69198465+TobiasHarrill@users.noreply.github.com>
2021-01-24 20:18:43 -0500

    Add files via upload

---
+++ project/index.php
@@ -0,0 +1,22 @@
+<?php include 'view/header.php'; ?>
+<style>
+  <?php include "main.css" ?>
+</style>
+<link rel="stylesheet" type="text/css" href="../main.css" />
+<main>
+    <nav>
+        
+    <h2>Customers</h2>
+    <ul>
+        <li><a href="view/product_view.php">Catalog</a></li>
+        <li><a href="under_construction.php">Register Product</a></li>
+        <li><a href="account/account_login_register.php">Login/Register</a></li>
+        <li><a href="cart/cart_view.php">Cart</a></li>
+        <li><a href="image_upload">Upload Images</a></li>
+        
+        
+    </ul>
+    
+    </nav>
+</section>
+<?php include 'view/footer.php'; ?>
